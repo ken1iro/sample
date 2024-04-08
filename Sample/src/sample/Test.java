7d4c52660e005c2ac4bc10d9b8b36436322ef9fd
ken1iro <kenichiro.okayasu.k361@dirbato.co.jp>
2024-04-08 12:27:42 +0900

    最初のコミット

---
+++ Sample/src/sample/Test.java
@@ -0,0 +1,16 @@
+package sample;
+
+public class Test {
+    
+    // mainメソッドはプログラムの最初に動作します。
+    public static void main(String[] args) {
+        /* コメントアウト
+        System.out.println("Kiramex");
+        System.out.println("Java");
+        */
+        System.out.println("kiramex");
+        System.out.println("Jaba");
+        System.out.println("学習を始めましょう");
+    }
+
+}
